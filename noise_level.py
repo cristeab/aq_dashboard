31f8df84ef87a19cef20c8405e7807f7d298f161
Bogdan Cristea <cristeab@gmail.com>
2025-07-29 17:09:38 +0300

    reset respeaker lite before listening on the mic

--- noise_level.py
+++ noise_level.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 from noise_detector.noise_detector import NoiseDetector
+from noise_detector.reset_respeaker import reset_respeaker_lite
 from persistent_storage import PersistentStorage
 from logger_configurator import LoggerConfigurator
+import time
 
 
-noise_detector = NoiseDetector()
-noise_detector.logger = LoggerConfigurator.configure_logger("NoiseDetector")
+logger = LoggerConfigurator.configure_logger("NoiseDetector")
+noise_detector = NoiseDetector(logger)
 persistent_storage = PersistentStorage()
 noise_detector.set_noise_callback(
     lambda timestamp, noise_level_db: persistent_storage.write_noise_level(timestamp, noise_level_db)
 )
+reset_respeaker_lite()
+time.sleep(2)
 noise_detector.run()
