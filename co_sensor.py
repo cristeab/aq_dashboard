f1622e50fec339414bee6c6e87bfc699ee13fe11
Bogdan Cristea <cristeab@gmail.com>
2026-01-01 13:51:58 +0200

    add script to read CO concentration

---
+++ co_sensor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from winsen_ze07co.carbon_monoxide_detector import CarbonMonoxideDetector
+from persistent_storage import PersistentStorage
+from logger_configurator import LoggerConfigurator
+import argparse
+from datetime import datetime, timezone
+import time
+
+
+parser = argparse.ArgumentParser(description='Script to read CO concentration')
+parser.add_argument('port', help='USB port to use (e.g., /dev/ttyACM0)')
+args = parser.parse_args()
+
+logger = LoggerConfigurator.configure_logger("CoSensor")
+co_sensor = CarbonMonoxideDetector(port=args.port, logger=logger)
+persistent_storage = PersistentStorage()
+while True:
+    timestamp = datetime.now(timezone.utc)
+    co_ppm = co_sensor.get_co_ppm()
+    if co_ppm is not None:
+        persistent_storage.write_co_data(timestamp, co_ppm)
+        local_time = timestamp.astimezone().strftime('%d/%m/%Y, %H:%M:%S')
+        print(f'Timestamp: {local_time}, CO: {co_ppm:.1f} PPM', flush=True)
+    time.sleep(0.1)
