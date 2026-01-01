ff3864b97632f66fbe503ac9082d0a2352874562
Bogdan Cristea <cristeab@gmail.com>
2026-01-01 18:07:02 +0200

    using QA mode to read data from CO sensor

--- co_sensor.py
+++ co_sensor.py
@@ -3,6 +3,7 @@
 from winsen_ze07co.carbon_monoxide_detector import CarbonMonoxideDetector
 from persistent_storage import PersistentStorage
 from logger_configurator import LoggerConfigurator
+from constants import SLEEP_DURATION_SECONDS
 import argparse
 from datetime import datetime, timezone
 import time
@@ -13,13 +14,16 @@ parser.add_argument('port', help='USB port to use (e.g., /dev/ttyACM0)')
 args = parser.parse_args()
 
 logger = LoggerConfigurator.configure_logger("CoSensor")
-co_sensor = CarbonMonoxideDetector(port=args.port, logger=logger)
 persistent_storage = PersistentStorage()
+
+co_sensor = CarbonMonoxideDetector(port=args.port, logger=logger)
+co_sensor.set_qa_mode()
+
 while True:
-    timestamp = datetime.now(timezone.utc)
-    co_ppm = co_sensor.get_co_ppm()
+    co_ppm = co_sensor.get_qa_co_ppm()
     if co_ppm is not None:
+        timestamp = datetime.now(timezone.utc)
         persistent_storage.write_co_data(timestamp, co_ppm)
         local_time = timestamp.astimezone().strftime('%d/%m/%Y, %H:%M:%S')
         print(f'Timestamp: {local_time}, CO: {co_ppm:.1f} PPM', flush=True)
-    time.sleep(0.1)
+    time.sleep(SLEEP_DURATION_SECONDS)
